1384a473d77137e3596a4ede64efbaff89b0ac7c
pvwnthem <pvwn.them@gmail.com>
2023-01-15 18:18:23 -0500

    add jwt helper

---
+++ src/utils/jwt.py
@@ -0,0 +1,11 @@
+import jwt
+import tinydb
+class Jwt:
+    def __init__(self, instances):
+        self.instances = instances
+
+    
+    def jwt_decode(self):
+        """Decode a JWT token."""
+        User = tinydb.Query()
+        print(jwt.decode(self.instances["db"].search(User.token.exists())[0]["token"], "NATEHIGGERS", algorithms=["HS256"]))
