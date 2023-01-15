5fafd4b3255f1f4e3f84f4870c7f675fcff746d7
pvwnthem <pvwn.them@gmail.com>
2023-01-15 14:26:52 -0500

    start route, add bcryptjs

--- server/routes/auth/register.ts
+++ server/routes/auth/register.ts
@@ -2,3 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import Invite from '../../models/invite.model';
 import User from '../../models/user.model';
+import bcrypt from 'bcryptjs';
+mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+
+})
