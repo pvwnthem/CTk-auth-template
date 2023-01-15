a6f8da20b07e9fd5fe8c1c15f7c6457c23668b09
pvwnthem <pvwn.them@gmail.com>
2023-01-15 14:19:03 -0500

    remove base route and add .env to gitignore

--- server/routes/invite.ts
+++ server/routes/invite.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 
 export default router;
