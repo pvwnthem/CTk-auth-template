9f9d6dcd18ec0a62dfbfe2bacf9ee9e8c855a92c
pvwnthem <pvwn.them@gmail.com>
2023-01-15 17:08:45 -0500

    jsondb

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,10 @@
 from utils.harvesthwid import harvest
 from gui import gui, splash_screen
 from gui.auth import login, register
-import pickledb
+import tinydb
 # register needs hwid passed in
 
-x = pickledb.load('data.db', False)
+x = tinydb.TinyDB("db.json")
 instances = {"gui": gui.Gui, "splash_screen": splash_screen.Splash, "login": login.Login, "register": register.Register, "db": x}
 
 
