7e83682981e125c5a2219a2ef18db535f4a4722d
pvwnthem <pvwn.them@gmail.com>
2023-01-15 14:18:15 -0500

    add invite.model

--- server/models/invite.model.ts
+++ server/models/invite.model.ts
@@ -1,1 +1,12 @@
 import mongoose from 'mongoose';
+
+const inviteSchema = new mongoose.Schema({
+    invite: {
+        type: String,
+        required: true,
+        unique: true
+    },
+})
+
+const Invite = mongoose.model('Invite', inviteSchema) || mongoose.model('Invite')
+
