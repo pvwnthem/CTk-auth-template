8b2bb5dc190b4cf6554822789004f123fbd71a1c
pvwnthem <pvwn.them@gmail.com>
2023-01-15 14:21:55 -0500

    add validate route

--- server/routes/invite.ts
+++ server/routes/invite.ts
@@ -6,6 +6,20 @@ mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true, useUnifiedTopo
 
 const router = express.Router();
 
+router.get('/validate', async (req, res) => {
+    const invite = req.query.invite
+    if (!invite) {
+        res.status(400).send('No invite code provided')
+    }
+    const inviteCode = await Invite.findOne({ invite: invite })
+    if (!inviteCode) {
+        res.status(400).send('Invalid invite code')
+    }
+    res.status(200).send('Valid invite code')
+})
+
+
+
 
 
 
