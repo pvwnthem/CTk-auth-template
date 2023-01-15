d3e13bfbef8e7939762f6b6bc75054666315396f
pvwnthem <pvwn.them@gmail.com>
2023-01-15 14:20:44 -0500

    mongoose.connect

--- server/routes/invite.ts
+++ server/routes/invite.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import Invite from '../models/invite.model';
 
+mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
 
 const router = express.Router();
 
