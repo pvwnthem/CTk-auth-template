de1320e379baacc6b1204bc494629ddee5f3f973
pvwnthem <pvwn.them@gmail.com>
2023-01-15 13:15:56 -0500

    start splash_screen

---
+++ src/gui/splash_screen.py
@@ -0,0 +1,33 @@
+import customtkinter as ctk
+import tkinter as tk
+import tkinter.messagebox as mbox
+
+ctk.set_appearance_mode("Dark") # Set the apperance mode to dark
+ctk.set_default_color_theme("blue") # Set the default color theme to blue
+
+
+class Gui(ctk.CTk): # The class that will be the main window of the application
+
+    WIDTH = 780 # The size of the window
+    HEIGHT = 520 # The size of the window 
+
+    def __init__(self): # The constructor of the class
+        super().__init__() # Call the super class constructor
+
+        self.title("Person Generator And Manager By @pvwn") # Set the title of the window
+        self.geometry(f"{self.WIDTH}x{self.HEIGHT}") # Set the size of the window
+        #self.protocol("WM_DELETE_WINDOW", self.on_closing) # When the user clicks the X button on the window
+
+        label = ctk.CTkLabel(master=self, text="Welcome" , font=("Arial", 20))
+        label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    gui = Gui() # Create an instance of the Gui class
+    gui.mainloop() # Start the mainloop of the application
