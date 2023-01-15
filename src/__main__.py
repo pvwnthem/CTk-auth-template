a7396cce9ec9846e3c21f83d9d2ecc9e556df532
pvwnthem <pvwn.them@gmail.com>
2023-01-15 16:10:32 -0500

    fix instances

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,11 @@
 from utils.harvesthwid import harvest
+from utils.sqlitemod import SqliteMod
 from gui import gui, splash_screen
 from gui.auth import login, register
 
 # register needs hwid passed in
 
-instances = {"gui": gui.Gui(), "splash_screen": splash_screen.Splash(), "login": login.Login(), "register": register.Register(harvest())}
+instances = {"gui": gui.Gui, "splash_screen": splash_screen.Splash, "login": login.Login, "register": register.Register}
 
+SqliteMod().create_table("user", "username text, password text, hwid text, invite text")
 
