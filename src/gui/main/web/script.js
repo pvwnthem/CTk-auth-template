f494daf53309357920072ce98eb6289c52687add
pvwnthem <pvwn.them@gmail.com>
2023-01-16 00:54:12 -0500

    set value

--- src/gui/main/web/script.js
+++ src/gui/main/web/script.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     const user = eel.gud()().then((user) => {
-        
+        var name = document.getElementById("name")
+        name.innerHTML = user.user.username
         console.log(user)
 
     })
