5e50bb63817f7c80b14223ce61874a958723ca5f
pvwnthem <pvwn.them@gmail.com>
2023-01-16 00:38:31 -0500

    final commit b4 tests

--- src/gui/main/gui.py
+++ src/gui/main/gui.py
@@ -8,14 +8,12 @@ class Gui:
         self.instances = instances
         
     def mainloop(self):
-
         dirname = os.path.dirname(__file__)
         eel.init(os.path.join(dirname, "web/"))
+        @eel.expose
+        def gud(self):
+            return self.instances["jwt"]().jwt_decode(self)
+        
         eel.start("index.html")
-    @eel.expose
-    def gud(self):
-        return self.instances["jwt"]().jwt_decode(self)
+    
 
-
-if __name__ == "__main__":
-    Gui({}).mainloop()
