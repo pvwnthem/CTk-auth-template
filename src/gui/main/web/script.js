9b2726375b75c94aba1577119aeabcea366be871
pvwnthem <pvwn.them@gmail.com>
2023-01-16 00:57:42 -0500

    dont console.log

--- src/gui/main/web/script.js
+++ src/gui/main/web/script.js
@@ -2,7 +2,6 @@ window.onload = () => {
     const user = eel.gud()().then((user) => {
         var name = document.getElementById("username")
         name.innerHTML = user.user.username
-        console.log(user)
 
     })
     
